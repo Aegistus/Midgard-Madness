544eb14fb11aa145ed033d240bc9adb9972864b6
Aegistus <joe.ballanders@gmail.com>
2021-03-05 16:10:49 -0500

    Fixed NPC Floating/Falling at Random Times

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -8,8 +8,6 @@ public class CameraController : MonoBehaviour
     public float ySensitivity = 1f;
     public Transform lookTarget;
 
-    private bool camMoveEnabled = true;
-
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -18,12 +16,9 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
-        if (camMoveEnabled)
-        {
+
             transform.LookAt(lookTarget);
             transform.RotateAround(lookTarget.position, Vector3.up, Input.GetAxis("Mouse X") * xSensitivity * Time.deltaTime);
             transform.RotateAround(lookTarget.position, -transform.right, Input.GetAxis("Mouse Y") * ySensitivity * Time.deltaTime);
-        }
-        camMoveEnabled = Cursor.visible ? false : true;
     }
 }
