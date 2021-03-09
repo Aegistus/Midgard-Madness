5f01c858f262d8e26156fce2f72ceb26ac0f136e
Aegistus <joe.ballanders@gmail.com>
2021-03-09 18:42:48 -0500

    Add Health Bar

---
+++ Assets/Scripts/UI/HealthBarUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarUI : MonoBehaviour
+{
+    public Transform healthBar;
+
+    private AgentHealth agent;
+
+
+    private void Awake()
+    {
+        agent = GetComponentInParent<AgentHealth>();
+    }
+
+    private void Update()
+    {
+        healthBar.localScale = new Vector3(agent.CurrentHealth / 100f, 1, 1);
+    }
+}
---
+++ Assets/Scripts/UI/LookAtCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    Camera main;
+
+    private void Start()
+    {
+        main = Camera.main;
+    }
+
+    void Update()
+    {
+        transform.LookAt(main.transform);
+    }
+}
