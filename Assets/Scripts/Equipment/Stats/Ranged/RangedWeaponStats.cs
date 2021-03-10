af696b4a793b0c838176cf83e0ac7ea385112f3f
Aegistus <joe.ballanders@gmail.com>
2021-03-10 11:55:43 -0500

    Fix Bow Walk and Run Animations

---
+++ Assets/Scripts/Equipment/Stats/Ranged/RangedWeaponStats.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewRangedWeaponStats", menuName = "Ranged Weapon Stats", order = 2)]
+public class RangedWeaponStats : WeaponStats
+{
+    public GameObject projectile;
+}
