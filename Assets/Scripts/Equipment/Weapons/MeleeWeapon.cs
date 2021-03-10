f8e9acda941a2c33b44bd7df89f8c5b6cb878b82
Aegistus <joe.ballanders@gmail.com>
2021-03-09 19:41:18 -0500

    Implement Blocking and Blocking Effects

--- Assets/Scripts/Equipment/Weapons/MeleeWeapon.cs
+++ Assets/Scripts/Equipment/Weapons/MeleeWeapon.cs
@@ -47,14 +47,26 @@ public class MeleeWeapon : Weapon
         if (inDamageState)
         {
             AgentHealth health = other.GetComponentInParent<AgentHealth>();
-            // check to make sure not hitting self or multi-hitting one agent
-            if (health != null && !hitAgents.Contains(health) && !transform.IsChildOf(health.transform))
+            Agent agent = other.GetComponentInParent<Agent>();
+            if (agent.CurrentState.GetType() != typeof(Blocking))
             {
-                health.Damage(stats.damage * damageModifier);
-                hitAgents.Add(health);
-                AudioManager.instance.PlaySoundGroupAtPosition("Sword Hit", transform.position);
-                PoolManager.Instance.GetObjectFromPoolWithLifeTime(PoolManager.PoolTag.Blood, other.ClosestPoint(transform.position), Quaternion.identity, 3f);
+                // check to make sure not hitting self or multi-hitting one agent
+                if (health != null && !hitAgents.Contains(health) && !transform.IsChildOf(health.transform))
+                {
+                    health.Damage(stats.damage * damageModifier);
+                    hitAgents.Add(health);
+                    AudioManager.instance.PlaySoundGroupAtPosition("Sword Hit", transform.position);
+                    PoolManager.Instance.GetObjectFromPoolWithLifeTime(PoolManager.PoolTag.Blood, other.ClosestPoint(transform.position), Quaternion.identity, 3f);
+                }
             }
+            else
+            {
+                print("Attack Blocked");
+                ExitDamageState();
+                AudioManager.instance.PlaySoundGroupAtPosition("Sword Block", transform.position);
+                PoolManager.Instance.GetObjectFromPoolWithLifeTime(PoolManager.PoolTag.Spark, other.ClosestPoint(transform.position), Quaternion.identity, 1f);
+            }
+
         }
     }
 }
--- Assets/Scripts/Object Pooling/PoolManager.cs
+++ Assets/Scripts/Object Pooling/PoolManager.cs
@@ -53,7 +53,7 @@ public class PoolManager : MonoBehaviour
 
     public enum PoolTag
     {
-        Blood
+        Blood, Spark
     }
 
     PoolObject objectFromPool;
