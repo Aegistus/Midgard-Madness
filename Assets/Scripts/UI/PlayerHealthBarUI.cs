5ea7066a4ccabd5b572b960c81d71000670b137c
Aegistus <joe.ballanders@gmail.com>
2021-03-12 10:06:03 -0500

    Add Player Health Bar UI

---
+++ Assets/Scripts/UI/PlayerHealthBarUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthBarUI : MonoBehaviour
+{
+    public Transform healthBar;
+
+    private AgentHealth player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<AgentHealth>();
+    }
+
+    private void Update()
+    {
+        healthBar.localScale = new Vector3(player.CurrentHealth / 100f, 1, 1);
+    }
+}
