8f7dcf58a727d4be49c8cfe0ddc68ff71b526124
Aegistus <joe.ballanders@gmail.com>
2021-03-13 12:01:55 -0500

    Add AgentSounds Scriptable Object

---
+++ Assets/Scripts/Agent/Agent Audio/AgentSounds.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewAgentSounds", menuName = "Agent Sounds", order = 3)]
+public class AgentSounds : ScriptableObject
+{
+    public SoundGroup breathing;
+    public SoundGroup heavyBreathing;
+    public SoundGroup footsteps;
+    public SoundGroup attack;
+    public SoundGroup hit;
+    public SoundGroup death;
+}
