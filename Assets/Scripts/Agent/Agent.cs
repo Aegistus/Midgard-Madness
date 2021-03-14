24af2ac7863347ae290bce21f13edcf2329fa28c
Aegistus <joe.ballanders@gmail.com>
2021-03-14 17:48:25 -0400

    Finish Ruined Tower and Fix Overactive Fall Animation

--- Assets/Scripts/Agent/Agent.cs
+++ Assets/Scripts/Agent/Agent.cs
@@ -71,7 +71,7 @@ public class Agent : MonoBehaviour
 
     public bool IsGrounded()
     {
-        if (Physics.BoxCast(transform.position, Vector3.one / 15, Vector3.down, transform.rotation, .5f, groundLayer))
+        if (Physics.BoxCast(transform.position, Vector3.one / 20, Vector3.down, transform.rotation, .75f, groundLayer))
         {
             return true;
         }
