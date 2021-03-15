099e21459f7c721e59dc8377f943c826bb714a64
Aegistus <joe.ballanders@gmail.com>
2021-03-14 21:32:47 -0400

    Add Bird Noises to Trees

---
+++ Assets/Scripts/Environment/BirdTree.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdTree : MonoBehaviour
+{
+    public List<AudioClip> birdsongs;
+    public float minInterval = 5f;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        StartCoroutine(PlayBirdSong());
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private IEnumerator PlayBirdSong()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.value * 40f + minInterval);
+            audioSource.clip = birdsongs[(int)(Random.value * birdsongs.Count)];
+            audioSource.Play();
+        }
+    }
+}
