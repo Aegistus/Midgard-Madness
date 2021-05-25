a72bb4aea3809f7983fcac7d21f0be8910c3217b
Aegistus <joe.ballanders@gmail.com>
2021-05-25 15:30:28 -0400

    Fix NPCs Not Running When Chasing

--- Assets/Scripts/Controllers/NPC/States/AIChasing.cs
+++ Assets/Scripts/Controllers/NPC/States/AIChasing.cs
@@ -31,6 +31,7 @@ public class AIChasing : NPCState
                 new ConditionNode(() => Node.ConvertToState(controller.Target != null)), // has target
                 new ActionNode(() => controller.SetDestination(controller.Target.position)), // set target
                 new ActionNode(() => agent.Forwards = true), // move to target
+                new ActionNode(() => agent.Run = true)
             }),
         });
     }
